8c50b5c9005f6830659602f15dcc68d5b2397db7
emanb29 <ebgamer29@gmail.com>
2015-05-23 22:43:44 -0700

    Even more UI improvement, implementing mines in cloud code

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,22 +1,44 @@
 'use strict';
 Parse.initialize('QoVOsIEyAMZ7g5wF1RSCblOIY7VieBl4O5DFmLT4', 'SbF6YcSmy3ykFEUh9ANXMOLUOSdGg9w5E1J0mLCX');
 
-var $toolbar =
-  $('<div class="CodeLumpHolder">' +
-    '<ul class="CodeLumpToolBar">' +
-      '<li><button class="actionBtn">TripMine</button></li>' +
-      '<li><button class="actionBtn">Case</button></li>' +
-      '<li><button class="actionBtn">Documents</button></li>' +
-      '<li><span class="cash">$123</span></li>' +
-      '<li><span class="armor">8</span></li>' +
-    '</ul>' +
-    '</div>');
+var $toolbar = $('<div id="CodeLumpTemplateShell" class="CodeLumpHolder"></div>');
 $toolbar.prependTo('body');
-//animate toolbar slide right
+var huddata = {
+  mines: 0,
+  cases: 0,
+  docs: 0,
+  cash: 500,
+  armor: 8
+};
+var hud = new Ractive({
+  // The `el` option can be a node, an ID, or a CSS selector.
+  el: '#CodeLumpTemplateShell',
 
+  // We could pass in a string, but for the sake of convenience
+  // we're passing the ID of the <script> tag above.
+  template:
+  '<ul class="CodeLumpToolBar">' +
+    '<li><button class="actionBtn mineBtn">' +
+      '<img src="'+chrome.extension.getURL('images/tripwire-48.png')+'" alt=""/>' +
+      '<span class="count mineCount">{{mines}}</span></button></li>' +
+    '<li><button class="actionBtn caseBtn">' +
+      //'<img src="'+chrome.extension.getURL('images/case-48.png')+'" alt=""/>' +
+      '<span class="count caseCount">{{cases}}</span></button></li>' +
+    '<li><button class="actionBtn docBtn">' +
+      //'<img src="'+chrome.extension.getURL('images/docs-48.png')+'" alt=""/>' +
+      '<span class="count docCount">{{docs}}</span></button></li>' +
+    '<li><span class="cash">${{cash}}</span></li>' +
+    '<li><button class="actionBtn" disabled><span class="armor count">{{armor}}</span></button></li>' +
+  '</ul>',
+
+  // Here, we're passing in some initial data
+  data: huddata,
+  magic: true
+});
+//animate toolbar slide right
 $('.CodeLumpToolBar').stop().animate({'right':'-58px'},600);
 
-$($toolbar).hover(
+$toolbar.hover(
   function () {
     $('.CodeLumpToolBar').stop().animate({'right':'0'},200);
   },
@@ -24,5 +46,12 @@ $($toolbar).hover(
     $('.CodeLumpToolBar').stop().animate({'right':'-58px'},200);
   }
 );
-
-console.log('\'Allo \'Allo! Content script');
+$('.mineBtn').click(function(){
+  if (huddata.mines<1) alert('You have no Proximity Mines!'); //TODO make less horrible and alerting
+  Parse.Cloud.run('placeMine', {url: document.url}).then(function(result) {
+    console.log(result.mine);
+    --huddata.mines;
+  }, function(error){
+    throw(error);
+  });
+});
--- server/cloud/main.js
+++ server/cloud/main.js
@@ -1,7 +1,39 @@
-
+var ProxMine = Parse.Object.extend('ProxMine');
 Parse.Cloud.define("hello", function(request, response) {
   response.success("CodeLump says, \"Stop poking around!\"");
 });
-Parse.Cloud.define("plantMine", function(request, response) {
-  response.success("CodeLump says, \"Stop poking around!\"");
+Parse.Cloud.define("placeMine", function(request, response) {
+  Parse.Cloud.useMasterKey();
+  var url = request.params.url;
+  var user = request.user;
+  if (user && user.get('proxMines') >= 1){
+    user.increment('proxMines', -1);
+    user.increment('assassinCP', 5);
+    user.save().then(function(user){
+      var mine = new ProxMine();
+      mine.set('placer', user);
+      mine.set('url', url);
+      mine.save().then(function(mine){
+        response.success({user: user, mine: mine});
+      }, function(error){
+        response.error(error);
+      });
+    }, function(error){
+      response.error(error);
+    });
+  }
+});
+Parse.Cloud.define("placeCase", function(request, response) {
+  Parse.Cloud.useMasterKey();
+  var user = request.user;
+  if (user && user.get('cases') >= 1) {
+
+  }
+});
+Parse.Cloud.define("placeDocuments", function(request, response) {
+  Parse.Cloud.useMasterKey();
+  var user = request.user;
+  if (user && user.get('docs') >= 1) {
+
+  }
 });
