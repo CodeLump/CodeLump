e756374c785750f8436e644e19d4eda347cebee5
emanb29 <ebgamer29@gmail.com>
2015-05-23 19:35:11 -0700

    More UI improvement

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,4 +1,4 @@
 'use strict';
-Parse.initialize("QoVOsIEyAMZ7g5wF1RSCblOIY7VieBl4O5DFmLT4", "SbF6YcSmy3ykFEUh9ANXMOLUOSdGg9w5E1J0mLCX");
+Parse.initialize('QoVOsIEyAMZ7g5wF1RSCblOIY7VieBl4O5DFmLT4', 'SbF6YcSmy3ykFEUh9ANXMOLUOSdGg9w5E1J0mLCX');
 
 console.log('\'Allo \'Allo! Popup');
